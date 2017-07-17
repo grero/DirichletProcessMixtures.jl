81f67b8626ae4053ee57547977c1b3aa70329d78
Roger Herikstad <roger.herikstad@gmail.com>
2017-07-17 16:14:00 +0800

    Adds tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,46 @@
-using DirichletProcessMixtures
 using Base.Test
+using StatsBase
+using DirichletProcessMixtures
+using Distributions
+using ConjugatePriors
 
-# write your own tests here
-@test 1 == 1
+import ConjugatePriors.NormalWishart
+
+function ball(N::Int64, x::Float64, y::Float64)
+    return randn(2, N) .+ [x, y]
+end
+
+function balley(M::Int64, R::Float64)
+    return hcat(ball(M, 0., 0.),
+            ball(M,  R,  R),
+            ball(M,  R, -R),
+            ball(M, -R,  R),
+            ball(M, -R, -R))
+end
+srand(1234)
+B = 60
+x = balley(B, 3.)
+xtest = balley(B, 3.)
+
+N = B * 5
+M = N
+
+prior = NormalWishart(zeros(2), 1e-7, eye(2) / 4.0, 4.0001)
+
+T = 20
+maxiter = 4000
+model = DirichletProcessMixtures.DPGMM(size(x,2), T, 1e-1, prior)
+
+lb_log = zeros(maxiter)
+tl_log = zeros(maxiter)
+
+niter = DirichletProcessMixtures.infer(model, x, maxiter, 1e-5)
+
+    
+z = DirichletProcessMixtures.map_assignments(model)
+C = countmap(z)
+@test C[1] == 123
+@test C[3] == 57
+@test C[4] == 59
+@test C[6] == 61
+@test niter == 117
